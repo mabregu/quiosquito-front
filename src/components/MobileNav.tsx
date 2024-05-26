43a9676f3aefaab54aa3162e8cb27e5691489f5c
N. Martin Abregu <nmabregu@gmail.com>
2024-05-26 13:14:28 -0300

    agrego layouts y mobile view

---
+++ src/components/MobileNav.tsx
@@ -0,0 +1,27 @@
+import { Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+
+const MobileNav = () => {
+    return (
+        <Sheet>
+            <SheetTrigger>
+                <Menu className="text-orange-500" />
+            </SheetTrigger>
+            <SheetContent>
+                <SheetTitle>
+                    <span>Welcome to the Mobile Nav</span>
+                </SheetTitle>
+                <Separator />
+                <SheetDescription className="flex">
+                    <Button className="flex-1 font-bold bg-orange-500">
+                        Log In
+                    </Button>
+                </SheetDescription>
+            </SheetContent>
+        </Sheet>
+    );
+}
+
+export default MobileNav;
